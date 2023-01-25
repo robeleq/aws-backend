5ed071ec778a5e358984ecdc166d5ea59731d09e
Robel Equbasilassie <robiki4life@gmail.com>
2023-01-25 18:49:31 +0300

    refactor: removed / endpoint

--- index.js
+++ index.js
@@ -3,14 +3,7 @@ const app = express()
 
 const PORT = process.env.PORT || 3001;
 
-
-app.get("/", (req, res) => {
-    res.json({
-        message: "Hello Backend Working..."
-    })
-})
-
-app.get("/api", (req, res) => {
+app.get("/api/message", (req, res) => {
     res.json({
         message: "API Backend..."
     })
