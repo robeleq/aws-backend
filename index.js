1bc91b0d7b5e3e3ba3404205cb4f6846b8ba1a31
Robel Equbasilassie <robiki4life@gmail.com>
2023-01-25 18:57:57 +0300

    refactor: rename /api/message to /v1/message endpoint

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 
 const PORT = process.env.PORT || 3001;
 
-app.get("/api/message", (req, res) => {
+app.get("/v1/message", (req, res) => {
     res.json({
         message: "API Backend..."
     })
