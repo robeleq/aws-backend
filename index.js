a5170ec48f91880b74090f7b2088abda435c214c
Robel Equbasilassie <robiki4life@gmail.com>
2023-01-27 11:03:27 +0300

    refactor: add custom message

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3001;
 
 app.get("/v1/message", (req, res) => {
     res.json({
-        message: "API Backend..."
+        message: "API Backend 2023..."
     })
 })
 
