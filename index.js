413760ba45035f06b4f249de362eff772f9f8e47
Robel Equbasilassie <robiki4life@gmail.com>
2023-09-06 16:20:39 +0300

    fix: API Message

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3001;
 
 app.get("/v1/message", (req, res) => {
     res.json({
-        message: "The API Backend 2023..."
+        message: "React API Backend 2023..."
     })
 })
 
